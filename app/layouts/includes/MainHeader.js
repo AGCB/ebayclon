ed91a871fb3da7ada336bbdd5b38fff63964dc63
agcb <oooalexoooo@gmail.com>
2024-05-26 23:55:50 -0700

    create searchbar

--- app/layouts/includes/MainHeader.js
+++ app/layouts/includes/MainHeader.js
@@ -1,11 +1,37 @@
 "use client"
 import Link from "next/link"
+import { AiOutlineSearch } from "react-icons/ai"
 
 const renderBayLogo = () => {
     return (
+        <div className="flex lg:justify-start justify-between gap-10 max-w-[1150] px-3 py-5 mx-auto">
         <Link href="/">
             <img width="120" src="/images/logo.svg" />
         </Link>
+        </div>
+    )
+}
+
+const renderSearchBar = () => {
+    return (
+        <>
+        <div className="w-full">
+            <div className="relative w-full">
+                <div className="flex items-center w-full">
+                    <div className="relative flex items-center border-2 border-gray-900 w-full p-2">
+                        <button className="flex items-center">
+                            <AiOutlineSearch size={22} />
+                        </button>
+                        <input
+                            className="w-full text-left placeholder-gray-400 text-sm pl-3 focus:outline-none"
+                            placeholder="search for anything"
+                            type="text">
+                        </input>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
 export default function MainHeader() {
@@ -13,10 +39,9 @@ export default function MainHeader() {
         <>
             <div id="MainHeader" className="border-b">
                 <div className="flex items-center justify-between w-full mx-auto max-w-[1200px]">
-                   <div className="flex items-center width-full bg-white">
-                        <div className="flex lg:justify-start justify-between gap-10 max-w-[1150] w-full px-3 py-5 mx-auto">
-                            {renderBayLogo()}
-                        </div>
+                   <div className="flex items-center w-full bg-white"> 
+                        {renderBayLogo()}
+                        {renderSearchBar()}
                    </div>
                 </div>
             </div>
