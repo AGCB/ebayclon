c559cc50413e1ad1a3fe455e47981b14d3102cc9
agcb <oooalexoooo@gmail.com>
2024-05-27 17:23:46 -0700

    create checkout section

--- app/cart/page.js
+++ app/cart/page.js
@@ -11,7 +11,49 @@ export default function Cart() {
         description: "It is a long established fact thatbution of letters, as opposed to using 'Content here, conttor",
         url: "https://picsum.photos/id/3",
         price: 2500 // EG 25.00
-}
+    }
+
+    const GoToCheckout = () => {
+        return (
+            <div
+                id="GoToCheckout"
+                className="
+                    md:w-[33%]
+                    absolute
+                    top-0
+                    right-0
+                    m-2">
+                <div className="bg-white p-4 border">
+                    <button className="
+                    flex
+                    items-center
+                    justify-center
+                    bg-blue-600
+                    w-full
+                    text-white
+                    font-semibold
+                    p-3
+                    rounded-full
+                    mt-4">
+                        Go To Checkout
+                    </button>
+                    <div className="flex items-center justify-between mt-4 text-sm mb-1">
+                        <div>Items (3)</div>
+                        <div>$ 9.00</div>
+                    </div>
+                    <div className="flex items-center justify-between mb-4 text-sm">
+                        <div>Shipping</div>
+                        <div>Free</div>
+                    </div>
+                    <div className="border-b border-gray-300"/>
+                    <div className="flex items-center justify-between mt-4 mb-1 text-lg font-semibold">
+                        <div>Subtotal</div>
+                        <div>$ 9.00</div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <>
@@ -24,6 +66,7 @@ export default function Cart() {
                         <div className="w-[65%]">
                             <CartItem key={product.id} product={product} />
                         </div>
+                        <GoToCheckout />
                     </div>
                 </div>
                 <SimilarProducts />
