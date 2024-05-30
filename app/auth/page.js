f92341d8863a6ad71ac6d70aae4dca1c3433d634
agcb <oooalexoooo@gmail.com>
2024-05-29 21:27:12 -0700

    create auth page

---
+++ app/auth/page.js
@@ -0,0 +1,34 @@
+"use client"
+// import { Auth } from '@supabase/auth-ui-react'
+// import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+// import { ThemeSupa, darkThemes, supabase } from '@supabase/auth-ui-shared'
+import Link from 'next/link'
+
+
+export default function AuthPage() {
+    // TODO: setup supabase
+    // const supabase = createClientComponentClient() 
+    return (
+        <div id="AuthPage" className='w-full min-h-screen bg-white'>
+            <div className='w-full flex items-center justify-center p-5 border-b-gray-300'>
+                <Link href="/" className='min-w-[170px]'>
+                    <img width="170" src="/images/logo.svg"/>
+                </Link>
+            </div>
+
+            <div className='w-full flex items-center justify-center p-5 border-b-gray-300'>
+                Login / Register
+            </div>
+
+            <div className='max-w-[400px] mx-auto px-2'>
+                {/* <Auth 
+                    onlyThirdPartyProviders
+                    redirectTo={`${window.location.origin}/auth/callback`}
+                    appearance={{theme: darkThemes}}
+                    providers={['google']}
+                    supabaseClient={supabase}
+                /> */}
+            </div>
+        </div>
+    )
+}
